5c6f957e02e7fbeb3cc5ca46bcc4aadd205753f3
GianlucaGentili <gentiligian@gmail.com>
2023-07-13 16:15:22 +0200

    cambiato login e email, sistemato frontend

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,56 +1,34 @@
-<x-layoutLogin>
-	<style>
-		body  {
-  background-image: url("https://ryansechrest.com/content/images/2022/08/how-to-set-an-animated-background-in-microsoft-teams-without-third-party-software.jpg");
-  background-color: #cccccc;
-  background-repeat: no-repeat;
-  background-size: cover;
-}
-	</style>
-			<div>
+<x-layout >
+	<body id="body-login">
+		<div class="margin-custom" ></div>
+		<center>
+			<div class="main">  	
+				<input type="checkbox" id="chk" aria-hidden="true">
 				
+				<div class="signup">
+					<form action="{{ route('register') }}" method="POST">
+						@csrf
+						{{-- <a href="{{route('homepage')}}"><img id="logo" class="img-fluid" src="https://th.bing.com/th/id/OIP.5EBcdJl3ADM-KQyAn25JaQHaH3?pid=ImgDet&rs=1" alt="Logo" width="18%"></a> --}}
+						<label for="chk" aria-hidden="true">Registrati</label>
+						<input id="input" type="text" name="name" placeholder="User name" required="">
+						<input id="input" type="email" name="email" placeholder="Email" required="">
+						<input id="input" type="password" name="password" placeholder="Password" required="">
+						<input id="input" type="password" name="password_confirmation" placeholder="Conferma password" required="">
+						<button id="button">Registrati</button>
+					</form>
+				</div>
+				
+				<div class="login">
+					<form action="{{ route('login') }}" method="POST">
+						@csrf
+						<label for="chk" aria-hidden="true">Login</label>
+						<input id="input" type="email" name="email" placeholder="Email" required="">
+						<input id="input" type="password" name="password" placeholder="Password" required="">
+						<button id="button">Login</button>
+					</form>
+				</div>
 			</div>
-	
-    <center>
-		<div style="height:160px;"></div>
-<div class="container m-5 d-flex justify-content-center align-items-center" id="container">
-	<div class="form-container sign-up-container">
-		<form action="{{ route('register') }}" method="POST">
-            @csrf
-			<a href="{{route('homepage')}}"><img id="logo" class="img-fluid" src="https://th.bing.com/th/id/OIP.5EBcdJl3ADM-KQyAn25JaQHaH3?pid=ImgDet&rs=1" alt="Logo" width="18%"></a>
-			<h2>Crea account</h2>
-			<input type="text" name="name" placeholder="Name" />
-			<input type="email" name="email" placeholder="Email" />
-			<input type="password" name="password" placeholder="Password" />
-            <input type="password" name="password_confirmation" placeholder="Conferma password" />
-			<button type="submit">Registrati</button>
-		</form>
-	</div>
-	<div class="form-container sign-in-container">
-		<form action="{{ route('login') }}" method="POST">
-			@csrf
-			<a href="{{route('homepage')}}"><img id="logo" class="img-fluid" src="https://th.bing.com/th/id/OIP.5EBcdJl3ADM-KQyAn25JaQHaH3?pid=ImgDet&rs=1" alt="Logo" width="18%"></a>
-			<h2>Accedi</h2>
-			<input type="email" name="email" placeholder="Email" />
-			<input type="password" name="password" placeholder="Password" />
-			<button type="submit">Accedi</button>
-		</form>
-	</div>
-	<div class="overlay-container">
-		<div class="overlay">
-			<div class="overlay-panel overlay-left">
-				<h1>Chi si rivede!</h1>
-				<p>Se sei già registrato effettua il login</p>
-				<button class="ghost" id="signIn">Accedi</button>
-			</div>
-			<div class="overlay-panel overlay-right">
-				<h3>Non sei registrato?</h3>
-				<p>Entra nella nostra community</p>
-				<button class="ghost" id="signUp">Registrati ora</button>
-			</div>
-		</div>
-	</div>
-</div>
-</center>
-
-</x-layoutLogin>
+		</center>
+		<div class="margin-custom"></div>
+	</body>
+</x-layout>
--- resources/views/careers.blade.php
+++ resources/views/careers.blade.php
@@ -1,60 +1,80 @@
 <x-layout>
-    <div class="margin-custom"></div>
-    <div class="container">
-        <div class="row">
-            <div class="col-12 col-md-6">
-                <h1 class="mb-4">Lavora con noi</h1>
+    <div class="margin-custom" ></div>
+    <center>
+        <h1 class="mb-5">Lavora con noi</h1>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-12">
+                    <h3>Lavora come amministratore</h3>
+                    <p class="mb-3">Che cosa farai? Avrai accesso senza limiti a tutte le funzionalità del sito</p>
+                    <h3>Lavora come revisore</h3>
+                    <p class="mb-3">Che cosa farai? Dovrai controllare che gli articoli rispettino le norme e non siano fake news</p>
+                    <h3>Lavora come scrittore</h3>
+                    <p class="mb-3">Che cosa farai? Offrirai le tue capacità di scrittura al nostro portale</p>
+                </div>
             </div>
-        </div>
-    </div>
-</div>
-<div class="container">
-    <div class="row">
-        <div class="col-12 col-md-6">
-            <h3>Lavora come amministratore</h3>
-            <p class="mb-2">Che cosa farai? Avrai accesso senza limiti a tutte le funzionalità del sito</p>
-            <h3>Lavora come revisore</h3>
-            <p class="mb-2">Che cosa farai? Dovrai controllare che gli articoli rispettino le norme e non siano fake news</p>
-            <h3>Lavora come scrittore</h3>
-            <p class="mb-2">Che cosa farai? Offrirai le tue capacità di scrittura al nostro portale</p>
-        </div>
-    </div>
-    
-    <div class="col-12 col-md-6">
-        @if($errors->any())
-        <div class="alert-danger alert">
-            <ul>
-                @foreach ($errors->all() as $error)
-                <li>
-                    {{$error}}
-                </li>
-                @endforeach
-            </ul>
-        </div>
-        @endif
-    </div>
-    
-    <form action="{{route('careers.submit')}}" method="post" enctype= "multipart/form-data">
-        @csrf
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label mt-4">Seleziona il ruolo...</label>
-            <select name="role" id="role" class="form-control">
-                <option value="">Scegli qui</option>
-                <option value="admin">Amministratore</option>
-                <option value="revisor">Revisore</option>
-                <option value="writer">Redattore</option>
-            </select>
-        </div>
-        <div class="mb-3">
-            <label for="email" class="form-label">Email</label>
-            <input type="email" name="email" class="form-control" id="email" value="{{old('email') ?? Auth::user()->email}}">
-        </div>
-        <div class="mb-3">
-            <label for="message" class="form-label">Parlaci di te</label>
-            <textarea name="message" id="message" cols="30" rows="7" class="form-control">{{old('message')}}</textarea>
-        </div>
-        <button type="submit" class="btn btn-primary">Invia la tua candidatura</button>
-    </form>
-</div>
-
+            <div class="col-12 col-md-6">
+                @if($errors->any())
+                <div class="alert-danger alert">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                        <li>
+                            {{$error}}
+                        </li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif
+            </div>
+            <div class="background">
+                <div class="container container-mail">
+                    <div class="screen">
+                        <div class="screen-header">
+                            <div class="screen-header-left">
+                                <div class="screen-header-button close"></div>
+                                <div class="screen-header-button maximize"></div>
+                                <div class="screen-header-button minimize"></div>
+                            </div>
+                            <div class="screen-header-right">
+                                <div class="screen-header-ellipsis"></div>
+                                <div class="screen-header-ellipsis"></div>
+                                <div class="screen-header-ellipsis"></div>
+                            </div>
+                        </div>
+                        <div id="container" class="screen-body">
+                            <div class="screen-body-item left">
+                                <div class="app-title">
+                                    <span>CONTATTACI</span>
+                                </div>
+                            </div>
+                            <div class="screen-body-item">
+                                <form action="{{route('careers.submit')}}" method="post" enctype= "multipart/form-data">
+                                    @csrf
+                                    <div class="app-form">
+                                        <div class="app-form-group">
+                                            <input class="app-form-control input-mail" type="email" id="email" name="email" placeholder="EMAIL" value="{{old('email') ?? Auth::user()->email}}">
+                                        </div>
+                                        <div class="app-form-group">
+                                            <select name="role" id="role" class=" app-form-control">
+                                                <option value="">Seleziona il ruolo</option>
+                                                <option value="admin">Amministratore</option>
+                                                <option value="revisor">Revisore</option>
+                                                <option value="writer">Redattore</option>
+                                            </select>
+                                        </div>
+                                        <div class="app-form-group message">
+                                            {{-- <input class="app-form-control input-mail" placeholder="MESSAGE"> --}}
+                                            <textarea name="message" id="message" cols="30" rows="7" placeholder="Messaggio" class="app-form-control">{{old('message')}}</textarea>
+                                        </div>
+                                        <div class="app-form-group buttons">
+                                            <button class="app-form-button button-mail fs-5" type="submit">INVIA</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </center>
 </x-layout>
